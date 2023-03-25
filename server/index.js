56bfedb786982f4c1a599f5f23f248dbc0dd5450
markspooner1 <105259793+markspooner1@users.noreply.github.com>
2023-03-25 14:59:37 -0400

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -8,28 +8,47 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import postRoutes from "./routes/posts.js"
-import authRoutes from "./routes/auth.js"
+import postRoutes from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-import {register} from "./controllers/auth.js"
-import {createPost} from "./controllers/posts.js"
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./Models/user.js"
-import Post from "./Models/Post.js";
-import {users, posts} from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__filename, __dirname);
 dotenv.config();
 const app = express();
+
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://ratemycourses.onrender.com"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+app.get('/pre-warm', (req, res) => {
+  // Add any necessary initialization code here
+  res.send('Server pre-warmed');
+});
+
+
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+
 //Set directory of assets, its a static directory
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
@@ -47,35 +66,39 @@ const storage = multer.diskStorage({
 //saves
 const upload = multer({ storage });
 
-
-//Authentication vs Authorization
-// Authentication: registering + logging in
-// Authorization: when we want to make sure that an action taken by a user is valid
-//Auth register
-//For when a user wants to register
-//user picture uploaded a picture to our public/assets folder
-//then we call our register middleware in controllers folder
-//grab picture property in http call
-app.post("/auth/register", upload.single("picture"), register) 
-app.post("/posts", verifyToken, upload.single("picture"), createPost);
 //Routes
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+//Auth register
+//For when a user wants to register
+//user picture uploaded a picture to our public/assets folder
+//then we call our register middleware in controllers folder
+//grab picture property in http cal;
+
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
 /* MONGOOSE SETUP */
 //Connect to our database from Node server
 const PORT = process.env.PORT || 3001;
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
+    
+
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-     //User.insertMany(users);
+    //User.insertMany(users);
     // Post.insertMany(posts)
   })
   .catch((error) => console.log(`${error} did not connect`));
